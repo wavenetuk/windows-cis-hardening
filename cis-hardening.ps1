4a180f5cb224aa4a7eb6d51786683e83c904c3be
Paul Martin <paul.martin@wavenet.co.uk>
2026-07-07 13:17:26 +0100

    fix: update level parameter type to string in cis-hardening script

--- cis-hardening.ps1
+++ cis-hardening.ps1
@@ -36,8 +36,8 @@ param (
 
     [Parameter(Mandatory = $false, ParameterSetName = 'Default')]
     [ValidateNotNullOrEmpty()]
-    [ValidateSet( 1, 2, 3)]
-    [string] $level = 1,
+    [ValidateSet('1', '2', '3')]
+    [string] $level = '1',
 
     [Parameter(Mandatory = $false, ParameterSetName = 'Default')]
     [string[]] $excludeControls = @(),
@@ -944,7 +944,7 @@ process {
         }
 
         # import controls based on environment
-        $controls = Import-Csv -Path $controlsCSV | Where-Object { ($_.ENABLED -eq "ENABLED") -and ($_.$($environment) -eq "ENABLED") -and ($_.$($os) -eq "ENABLED") -and ([int]$_.Level -le $level) -and ($_.ControlID -notin $excludeControls) }
+        $controls = Import-Csv -Path $controlsCSV | Where-Object { ($_.ENABLED -eq "ENABLED") -and ($_.$($environment) -eq "ENABLED") -and ($_.$($os) -eq "ENABLED") -and ([int]$_.Level -le [int]$level) -and ($_.ControlID -notin $excludeControls) }
 
         # Registry section
         foreach ($control in ($controls | Where-Object { ($_.Type -eq "Registry") })) {
